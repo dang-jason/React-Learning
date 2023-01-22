d2e571939d7f67c8be0af96302e499cb35545d73
Jason Dang <jasondang619@gmail.com>
2023-01-22 10:56:53 -0600

    Added JSX code to make coding declarative

--- React-Basics/index.js
+++ React-Basics/index.js
@@ -1,2 +1,35 @@
-ReactDOM.render(<h1>Hi, my name is Jason. This will be my first go at React.</h1>, document.querySelector('#root'));
-ReactDOM.render(<ul><li>Item 1</li><li>Item 2</li></ul>, document.querySelector('#root1'));
+function Composable(){
+    return (
+        <p>React is composable.</p>
+    )
+}
+// JSX - creates things with objects, must only have one parent element in render.
+const page = (
+    <div>
+        <nav>
+            <h1>Limitless</h1>
+            <ul>
+                <li>Pricing</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+        <h2 className="header">Hi, my name is Jason. This will be my first go at React.</h2>
+        <Composable />
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+        </ul>
+    </div>
+)
+ReactDOM.render(
+    page,
+    document.querySelector('#root')
+)
+
+
+const header = document.createElement('h2');
+header.textContent = "This is imperative implementation, React lets you make it declarative."
+header.classList.add('header');
+document.querySelector('#root').appendChild(header);
+
