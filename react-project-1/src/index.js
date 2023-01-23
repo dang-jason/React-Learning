e0948c90ee8fa233d036821c5c9814c8c8cb7cf4
Jason Dang <jasondang619@gmail.com>
2023-01-23 10:08:07 -0600

    Created static page w/o styling

--- react-project-1/src/index.js
+++ react-project-1/src/index.js
@@ -4,4 +4,19 @@ import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<div></div>);
+
+const page = (
+  <div>
+    <img src="./react-logo.png" width="80px"></img>
+    <h1>Fun facts about react</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was orginially created by Jordan Walke</li>
+      <li>Has well over 100K stars on GIthub</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
+);
+
+root.render(page);
