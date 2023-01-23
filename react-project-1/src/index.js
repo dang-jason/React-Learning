2f9d6af1f157c340b4b50e4f79540facdc9db436
Jason Dang <jasondang619@gmail.com>
2023-01-23 10:02:37 -0600

    error fixed

--- react-project-1/src/index.js
+++ react-project-1/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <h1>!</h1>
-);
+root.render(<div></div>);
