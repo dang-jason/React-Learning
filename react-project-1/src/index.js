b4ff0527a765c859438ba3fb3fbb7d0e22cd1662
Jason Dang <jasondang619@gmail.com>
2023-01-26 08:57:07 -0600

    naming issue

--- react-project-1/src/index.js
+++ react-project-1/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
-import Header from "./components/Header";
+import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 
