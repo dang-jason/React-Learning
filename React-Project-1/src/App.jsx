ec3f0ec446daf5af31e40ab1a1210376d180fb6d
Jason Dang <jasondang619@gmail.com>
2023-01-28 09:57:11 -0600

    Styles and added main section

--- React-Project-1/src/App.jsx
+++ React-Project-1/src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 import Main from './components/Main';
 function App() {
 	return (
-		<div className="App">
+		<div className="app">
 			<Navbar />
 			<Main />
 		</div>
