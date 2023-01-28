72fdb567564b077768fb63a7eddc52e91756fe11
Jason Dang <jasondang619@gmail.com>
2023-01-28 11:54:42 -0600

    added and styles info section

--- Business-Card/src/components/Info.jsx
+++ Business-Card/src/components/Info.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import portrait from '../images/jason-coding.png';
+import linkedinLogo from '../images/linkedin.png';
+import mailLogo from '../images/mail.png';
 
 function Info() {
-	return <h1>Info Section</h1>;
+	return (
+		<header>
+			<img src={portrait} alt="Jason Dang" className="header--portrait" />
+			<h1 className="header--name">Jason Dang</h1>
+			<p className="header--nametag">Electrical and Computer Engineer</p>
+			<p className="header--website">Website tba</p>
+			<div className="socials">
+				<a className="social social--email" href="mailto:jasondang619@gmail.com" target="_blank">
+					<img src={mailLogo} alt="email" className="socials--emaillogo" />
+					<p>Email</p>
+				</a>
+				<a className="social social--linkedin" href="https://www.linkedin.com/in/jasdang/" target="_blank">
+					<img
+						src={linkedinLogo}
+						alt="linkedin"
+						className="socials--linkedinlogo"
+					/>
+					<p>LinkedIn</p>
+				</a>
+			</div>
+		</header>
+	);
 }
 
 export default Info;
