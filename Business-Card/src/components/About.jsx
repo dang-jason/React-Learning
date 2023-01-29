36a0f64ec036682f8a7d19b4a8db407f171ebb6d
Jason Dang <jasondang619@gmail.com>
2023-01-29 12:13:23 -0600

    Styles and added about and interest sections

--- Business-Card/src/components/About.jsx
+++ Business-Card/src/components/About.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 function About() {
-	return <h1>About Section</h1>;
+	return (
+		<main>
+			<h4 className="about--title">About</h4>
+			<p className="about--text">
+				I am a sophomore at the University of Texas at Austin majoring in
+				Electrical and Computer Engineering. I want to dive into the world of
+				software development and learn all there is to know. I am a fanatic on
+				trying to learn.
+			</p>
+		</main>
+	);
 }
 
 export default About;
--- Business-Card/src/components/Interests.jsx
+++ Business-Card/src/components/Interests.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function Interests() {
-	return <h1>Interests Section</h1>;
+	return (
+		<div>
+			<h4 className="interests--title">Interests</h4>
+			<p className="interests--text">
+				Reading. Rock climbing. Working out. Coding. Time with my friends.
+				Cooking. The internet. Learning something daily. Living. Feeling.
+			</p>
+		</div>
+	);
 }
 
 export default Interests;
