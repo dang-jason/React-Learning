1e04e723e711fcf1aae323fbd8cc2cd0da6aa3d6
Jason Dang <jasondang619@gmail.com>
2023-05-02 18:16:04 -0500

    Adding the cards using props and mapping

--- AirBnB/src/App.jsx
+++ AirBnB/src/App.jsx
@@ -2,16 +2,23 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-/*
-Challenge: Build the Navbar component.
-Check the Figma file for the design specifics.
-*/
+import Card from './components/Card';
+
+import imgperson from './images/katie-zaferes.png';
 
 export default function App() {
 	return (
 		<div className="app--root">
 			<Navbar />
 			<Hero />
+			<Card
+				img={imgperson}
+				rating="5.0"
+				reviewCount={6}
+				country="USA"
+				title="Life Lessons With Katie Zaferes"
+				price={136}
+			/>
 		</div>
 	);
 }
