3421c0e646ef24cbb20d4b9f96608f14154ee4f9
Jason Dang <jasondang619@gmail.com>
2023-05-09 12:00:19 -0500

    fixed styling

--- AirBnB/src/App.jsx
+++ AirBnB/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Card from './components/Card';
-import data from './data.js'
+import data from './data.js';
 
 export default function App() {
 	const cards = data.map(person => {
